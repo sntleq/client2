f3b033d9bd546c0ce91de8ef76c4b36ab5135988
Yohhan <53113613+Yohhannn@users.noreply.github.com>
2025-05-24 12:21:44 +0800

    - Completed 80% Teacher UI

---
+++ Controllers/TeacherController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace Fresh_University_Enrollment.Controllers
+{
+    public class TeacherController : Controller
+    {
+        public ActionResult Dashboard()
+        {
+            return View("~/Views/Teacher/Dashboard.cshtml");
+        }
+        public ActionResult Classes()
+        {
+            return View("~/Views/Teacher/Classes.cshtml");
+        }
+    }
+}
