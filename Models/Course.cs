c93bab8be9240c83973c3f7e40650407329ed18a
Sonix-J <limosnerogabriel@gmail.com>
2025-05-26 21:16:53 +0800

    n/a

---
+++ Models/Course.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Fresh_University_Enrollment.Models;
+
+public class Course
+{
+    public string CrsCode { get; set; }
+    public string CrsTitle { get; set; }
+    public decimal CrsUnits { get; set; }
+    public int CrsLec { get; set; }
+    public int CrsLab { get; set; }
+
+    public string? PreqId { get; set; }  // Nullable because it's optional
+    public string CtgCode { get; set; }
+
+    // Navigation Properties
+    public Course Prerequisite { get; set; }  // Self-referencing foreign key
+    public CourseCategory Category { get; set; }
+    public ICollection<CurriculumCourse> CurriculumCourses { get; set; } = new List<CurriculumCourse>();
+}
---
+++ Models/CourseCategory.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Fresh_University_Enrollment.Models;
+
+public class CourseCategory
+{
+    public string CtgCode { get; set; }
+    public string CtgName { get; set; }
+
+    // Navigation Properties
+    public ICollection<Course> Courses { get; set; } = new List<Course>();
+}
