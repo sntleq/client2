f96895239dcdf34a447dedcdbb53150f97373bb2
Sonix-J <limosnerogabriel@gmail.com>
2025-05-27 22:02:14 +0800

    okay nani admin ata

--- Controllers/StudentEnrollment.cs
+++ Controllers/StudentEnrollment.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Web.Mvc;
 using Fresh_University_Enrollment.Models;
@@ -8,7 +9,7 @@ namespace Fresh_University_Enrollment.Controllers
 {
     public class StudentEnrollmentController : Controller
     {
-        private string connString = ConfigurationManager.ConnectionStrings["Enrollment"].ConnectionString;
+        private readonly string _connectionString = ConfigurationManager.ConnectionStrings["Enrollment"].ConnectionString;
 
         public ActionResult Student_Enrollment()
         {
@@ -19,51 +20,94 @@ namespace Fresh_University_Enrollment.Controllers
                 return RedirectToAction("Login", "Account");
             }
 
-            int studCode = Convert.ToInt32(sessionStudCode);
-            var student = new Student();
+            int studCode;
+            if (!int.TryParse(sessionStudCode.ToString(), out studCode))
+            {
+                return RedirectToAction("Login", "Account");
+            }
 
             try
             {
-                using (var conn = new NpgsqlConnection(connString))
+                // Load student data
+                var student = GetStudentById(studCode);
+                if (student == null)
                 {
-                    conn.Open();
-                    string query = "SELECT * FROM STUDENT WHERE STUD_CODE = @studCode";
-
-                    using (var cmd = new NpgsqlCommand(query, conn))
-                    {
-                        cmd.Parameters.AddWithValue("@studCode", studCode);
-
-                        using (var reader = cmd.ExecuteReader())
-                        {
-                            if (reader.Read())
-                            {
-                                student.Stud_Id = reader.GetInt32(reader.GetOrdinal("stud_id"));
-                                student.Stud_Lname = reader["stud_lname"]?.ToString();
-                                student.Stud_Fname = reader["stud_fname"]?.ToString();
-                                student.Stud_Mname = reader["stud_mname"]?.ToString();
-                                student.Stud_Dob = Convert.ToDateTime(reader["stud_dob"]);
-                                student.Stud_Contact = reader["stud_contact"]?.ToString();
-                                student.Stud_Email = reader["stud_email"]?.ToString();
-                                student.Stud_Address = reader["stud_address"]?.ToString();
-                                student.Stud_Code = Convert.ToInt32(reader["stud_code"]);
-                            }
-                            else
-                            {
-                                ViewBag.ErrorMessage = "Student not found.";
-                                return View("~/Views/Shared/Error.cshtml");
-                            }
-                        }
-                    }
+                    ViewBag.ErrorMessage = "Student not found.";
+                    return View("~/Views/Shared/Error.cshtml");
                 }
+
+                // Load programs for dropdown
+                var programs = GetProgramsFromDatabase();
+                ViewBag.Programs = programs;
+
+                // Return the view with student model and programs in ViewBag
+                return View("~/Views/Main/StudentEnroll.cshtml", student);
             }
             catch (Exception ex)
             {
                 ViewBag.ErrorMessage = $"Error loading student data: {ex.Message}";
                 return View("~/Views/Shared/Error.cshtml");
             }
+        }
 
-            // Use full path to ensure it finds the correct view
-            return View("~/Views/Main/StudentEnroll.cshtml", student);
+        private Student GetStudentById(int studCode)
+        {
+            using (var conn = new NpgsqlConnection(_connectionString))
+            {
+                conn.Open();
+                string query = "SELECT * FROM STUDENT WHERE STUD_CODE = @studCode";
+
+                using (var cmd = new NpgsqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@studCode", studCode);
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return new Student
+                            {
+                                Stud_Id = reader.GetInt32(reader.GetOrdinal("stud_id")),
+                                Stud_Lname = reader["stud_lname"]?.ToString(),
+                                Stud_Fname = reader["stud_fname"]?.ToString(),
+                                Stud_Mname = reader["stud_mname"]?.ToString(),
+                                Stud_Dob = Convert.ToDateTime(reader["stud_dob"]),
+                                Stud_Contact = reader["stud_contact"]?.ToString(),
+                                Stud_Email = reader["stud_email"]?.ToString(),
+                                Stud_Address = reader["stud_address"]?.ToString(),
+                                Stud_Code = Convert.ToInt32(reader["stud_code"])
+                            };
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private List<Program> GetProgramsFromDatabase()
+        {
+            var programs = new List<Program>();
+
+            using (var conn = new NpgsqlConnection(_connectionString))
+            {
+                conn.Open();
+                using (var cmd = new NpgsqlCommand("SELECT \"prog_code\", \"prog_title\" FROM \"program\"", conn))
+                {
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            programs.Add(new Program
+                            {
+                                ProgCode = reader.GetString(0),
+                                ProgTitle = reader.GetString(1)
+                            });
+                        }
+                    }
+                }
+            }
+            return programs;
         }
     }
 }
